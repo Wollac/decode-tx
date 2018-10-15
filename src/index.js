46d727e9fa198819ce0ef0a98b068ada59b00699
Wolfgang Welz <welzwo@gmail.com>
2018-10-15 18:02:40 +0200

    Create trivial app to convert transaction trytes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactJson from 'react-json-view';
+import { asTransactionObject } from '@iota/transaction-converter';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TransactionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trytes: '',
+      txObject: {}
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ trytes: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const txObject = this._parseTransaction(this.state.trytes);
+      this.setState({ txObject });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  _parseTransaction(trytes) {
+    const txObject = asTransactionObject(trytes);
+
+    txObject.timestamp = new Date(txObject.timestamp * 1000);
+
+    txObject.attachmentTimestamp = new Date(txObject.attachmentTimestamp);
+    txObject.attachmentTimestampLowerBound = new Date(
+      txObject.attachmentTimestampLowerBound
+    );
+    txObject.attachmentTimestampUpperBound = new Date(
+      txObject.attachmentTimestampUpperBound
+    );
+
+    return txObject;
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Convert Transaction Trytes</h2>
+        <h3>Transaction Trytes</h3>
+        <form onSubmit={this.handleSubmit}>
+          <textarea
+            id="trytesinput"
+            value={this.state.trytes}
+            onChange={this.handleChange}
+            cols={95}
+            rows={28}
+          />
+          <button>Convert</button>
+        </form>
+        <div>
+          <h3>Transaction Object</h3>
+          <ReactJson
+            src={this.state.txObject}
+            name={false}
+            theme="rjv-default"
+            collapseStringsAfterLength={81}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TransactionForm />, document.getElementById('root'));
