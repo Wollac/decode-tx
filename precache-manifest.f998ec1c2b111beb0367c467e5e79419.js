b04378aacff46b0c72d2be5dad1b6ddc6835734d
Wolfgang Welz <welzwo@gmail.com>
2019-07-05 20:00:41 +0200

    Updates

---
+++ precache-manifest.f998ec1c2b111beb0367c467e5e79419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6f64d0d4e70ebfd59d0cf204067fb0",
+    "url": "/decode-tx/index.html"
+  },
+  {
+    "revision": "f8e793af0e71ef489372",
+    "url": "/decode-tx/static/css/main.aeb06fc7.chunk.css"
+  },
+  {
+    "revision": "010a2320b02c288806a2",
+    "url": "/decode-tx/static/js/2.07da9a95.chunk.js"
+  },
+  {
+    "revision": "f8e793af0e71ef489372",
+    "url": "/decode-tx/static/js/main.f54b70a0.chunk.js"
+  },
+  {
+    "revision": "dad0058091737e8e5160",
+    "url": "/decode-tx/static/js/runtime~main.5dd5b37d.js"
+  }
+]);
